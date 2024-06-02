0120a948eca458076bce947d23b7f99ede4faac8
denerslemos <denerslemos@gmail.com>
2024-06-02 00:48:24 -0500

    fix

--- random_mixing.h
+++ random_mixing.h
@@ -6,8 +6,11 @@
 void MixEvents(bool use_centrality, int centrality_or_ntrkoff_int, int nEvt_to_mix, std::vector<int> ev_centrality, std::vector<int> ev_multiplicity, std::vector<double> vtx_z_vec, float vzcut, std::vector<std::vector<ROOT::Math::PtEtaPhiMVector>> Track_Vector, std::vector<std::vector<int>> Track_Chg_Vector, std::vector<std::vector<double>> Track_Eff_Vector, THnSparseD *histo_SS, THnSparseD *histo_SS3D, THnSparseD *histo_OS, THnSparseD *histo_OS3D, bool docostdptcut, bool do_hbt3d, bool dogamovcorrection, int systematic, TH1I* NeventsAss){
 
    int aux_n_evts = (int) vtx_z_vec.size(); // total number of events
-   
+   int tenper = (int)0.1*aux_n_evts;   
    for(int nevt_trg = 0; nevt_trg < aux_n_evts; nevt_trg++){ // first loop over all events
+
+	  if(nevt_trg == 0) cout << "Running ... " << endl;
+	  if(nevt_trg != 0 && (nevt_trg % tenper) == 0){cout << nevt_trg << " out of " << aux_n_evts << endl;} // % processed
 
       std::vector<ROOT::Math::PtEtaPhiMVector> Trk_nevt_trg_vec = Track_Vector[nevt_trg]; // track 4-vector for each trigger event
 	  std::vector<int> Trk_chg_nevt_trg_vec = Track_Chg_Vector[nevt_trg]; // track charge vector for each trigger event
